42bc31d03e8487ebc5a89a29c9244f2a89c784da
andrei <andrei@sampler.io>
2017-12-28 11:38:03 -0500

    Add basic external getters

--- server/app/Services/ExternalWordService.js
+++ server/app/Services/ExternalWordService.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const Env = use('Env');
+const fetch = require('node-fetch');
 
 /**
  * Words API wrapper service
  */
 class ExternalWordService {
 
+  // API Constants
   static get API_ENDPOINTS() {
     return {
       summary: ExternalWordService.API_ROOT + ':word',
@@ -15,19 +17,35 @@ class ExternalWordService {
   }
   static get DAILY_API_CALLS_LIMIT() { return 5000 }
   static get API_ROOT() { return 'https://wordsapiv1.p.mashape.com/words/' }
-  static get API_REQUEST_HEADERS() {
+
+  // Request constants
+  static get REQUEST_HEADERS() {
     return {
       'accept': 'application/json',
       'X-Mashape-Key': Env.get('X_MASHAPE_KEY')
     }
   }
-
-  getSummary(word) {
-
+  static get REQUEST_GET_CONFIG() {
+    return {
+      method: 'get',
+      headers: ExternalWordService.REQUEST_HEADERS
+    }
   }
 
-  getSynonyms(word) {
+  async getSummary(word) {
+    let response = await fetch(
+      ExternalWordService.API_ENDPOINTS.summary.replace(':word', word),
+      ExternalWordService.REQUEST_GET_CONFIG);
 
+    return await response.json()
+  }
+
+  async getSynonyms(word) {
+    let response = await fetch(
+      ExternalWordService.API_ENDPOINTS.synonyms.replace(':word', word),
+      ExternalWordService.REQUEST_GET_CONFIG);
+
+    return await response.json()
   }
 }
 
