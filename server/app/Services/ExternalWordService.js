b91a970782d98245b57466d5c2ae5fbe906f5445
andrei <andrei@sampler.io>
2017-12-29 12:06:20 -0500

    Add (nonfunctioning) throttling, add response parsing to getters

--- server/app/Services/ExternalWordService.js
+++ server/app/Services/ExternalWordService.js
@@ -15,6 +15,7 @@ class ExternalWordService {
       synonyms: ExternalWordService.API_ROOT + ':word/synonyms'
     }
   }
+  // TODO: Confirm this
   static get DAILY_API_CALLS_LIMIT() { return 5000 }
   static get API_ROOT() { return 'https://wordsapiv1.p.mashape.com/words/' }
 
@@ -32,20 +33,85 @@ class ExternalWordService {
     }
   }
 
+  constructor() {
+    this._checkApiLimit();
+  }
+
+  /**
+   * Fetches summary from the Words API
+   * @param {string} word
+   * @returns {Promise<Object>}
+   */
   async getSummary(word) {
     let response = await fetch(
       ExternalWordService.API_ENDPOINTS.summary.replace(':word', word),
       ExternalWordService.REQUEST_GET_CONFIG);
 
-    return await response.json()
+    return this._parseSummary(await response.json());
   }
 
+
+  /**
+   * Fetches synonyms from the Words API
+   * @param {string} word
+   * @returns {Promise<string[]>}
+   */
   async getSynonyms(word) {
     let response = await fetch(
       ExternalWordService.API_ENDPOINTS.synonyms.replace(':word', word),
       ExternalWordService.REQUEST_GET_CONFIG);
 
-    return await response.json()
+    return this._parseSynonyms(await response.json())
+  }
+
+
+  /**
+   * Parses relevant summary information
+   * @param {object} summaryResponse
+   * @returns {object}
+   * @private
+   */
+  _parseSummary(summaryResponse) {
+    let summary = {
+      name,
+      syllablesCount,
+      ultima
+    };
+
+    summary = Object.assign({}, summary, {
+      name: summaryResponse.word,
+      syllablesCount: (summaryResponse.syllables ? summaryResponse.syllables.count : null),
+    };
+
+    if (summaryResponse.pronunciation) {
+      let ipa = summaryResponse.pronunciation.all || summaryResponse.pronunciation;
+      summary.ultima = getUltima(ipa);
+    }
+
+    return summary;
+  }
+
+
+  /**
+   * Parses relevant synonyms information
+   * @param synonymsResponse
+   * @returns {string[]}
+   * @private
+   */
+
+  _parseSynonyms(synonymsResponse) {
+    // Pretty simple right now, but wanted to have a consistent get/parse pattern for each Words API endpoint
+    let synonyms = synonymsResponse.synonyms;
+    return synonyms;
+  }
+
+
+  _checkApiLimit() {
+    let apiCallsCount = 0;  // TODO: Fill this (probably Redis)
+
+    if (apiCallsCount >= ExternalWordService.DAILY_API_CALLS_LIMIT) {
+      throw "Reached API calls limit";
+    }
   }
 }
 
