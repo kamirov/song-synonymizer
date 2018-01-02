030d1a4cd220ad818d54636f0dd789628b40d7de
andrei <andrei@sampler.io>
2018-01-01 14:54:43 -1000

    Add Docker image to server, remove post definition from .env file

---
+++ server/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Replace Adonis HOST and PORT values (keeps us consistently defining ports in docker-compose file)
+sed -i -e "s/PORT=.*/PORT=$PORT/g" .env
+
+# Check for .env file
+if [[ -z "$(ls -A $PWD | grep .env)" ]]; then
+	echo "no .env file found."
+	exit 1
+fi
+
+npm install
+
+# Add symbolic link (needed to let "require" calls work, which are needed for Intellisense)
+ln -sf /app/app ./node_modules/App
+
+# start app
+source .env
+if [[ "$NODE_ENV" == "production" ]]; then
+	adonis serve
+else
+	adonis serve --dev
+fi
