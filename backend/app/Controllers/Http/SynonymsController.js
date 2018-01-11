9b5b0c8951a2878f62312db8d48f5ea73566126a
andrei <andrei@sampler.io>
2018-01-11 14:50:05 -0500

    Add word addition on synonymize call

--- backend/app/Controllers/Http/SynonymsController.js
+++ backend/app/Controllers/Http/SynonymsController.js
@@ -3,11 +3,18 @@
 const Logger = use('Logger');
 
 const SynonymService = use('App/Services/SynonymService');
+const WordStorageService = use('App/Services/WordStorageService');
 
 class SynonymController {
   async synonymize({request}) {
+    // Add new words (if any)
+    let text = request.post().text;
+    await (new WordStorageService).addNewWords(text);
+
+    // Synonymize
     let synonymService = new SynonymService;
-    synonymService.setFlags(request.post().flags);
+
+    synonymService.setFlags(JSON.parse(request.post().flags));
     return await synonymService.synonymize(request.post().text);
   }
 }
