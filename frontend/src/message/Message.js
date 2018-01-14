ca8802b7e69bd0e7f4e627c4f6da68f0f40bb3fa
andrei <andrei@sampler.io>
2018-01-14 13:43:39 -0500

    Don't close on-click while fetching

--- frontend/src/message/Message.js
+++ frontend/src/message/Message.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Snackbar from 'material-ui/Snackbar';
 
-const Message = ({open, content, onClick}) => {
+const Message = ({open, content, apiStatus, onClick}) => {
     return <Snackbar
         anchorOrigin={{ vertical: 'top', horizontal:'right' }}
         open={open}
-        onClose={onClick}
+        onClose={onClick(apiStatus)}
         SnackbarContentProps={{
             'aria-describedby': 'message-id',
         }}
--- frontend/src/message/MessageContainer.js
+++ frontend/src/message/MessageContainer.js
@@ -1,15 +1,19 @@
 import {connect} from 'react-redux'
 import Message from "./Message";
 import {setOpen} from "./messageActions";
+import apiConstants from "../api/apiConstants";
 
 const mapStateToProps = state => ({
     content: state.message.content,
-    open: state.message.open
+    open: state.message.open,
+    apiStatus: state.api.status
 })
 
 const mapDispatchToProps = dispatch => ({
-    onClick: () => {
-        dispatch(setOpen(false))
+    onClick: (apiStatus) => {
+        if (apiStatus !== apiConstants.STATUSES.FETCHING) {
+            dispatch(setOpen(false))
+        }
     }
 })
 
