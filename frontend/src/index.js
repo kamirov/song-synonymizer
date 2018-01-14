3102b6d820a4146bc511adcc3f598ba074774286
andrei <andrei@sampler.io>
2018-01-13 21:26:42 -0500

    Extract store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
 import App from './app/App';
-import RootReducer from './app/reducers';
 import registerServiceWorker from './registerServiceWorker';
-
-let store = createStore(RootReducer);
-
-const unsubscribe = store.subscribe(() =>
-    console.log('New State: ', store.getState())
-);
+import store from "./app/store";
 
 ReactDOM.render(
     <Provider store={store}>
